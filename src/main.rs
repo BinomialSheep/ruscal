30edd56f82bdfa5aede1ccbd232528cc935e453b
BinomialSheep <jose_jinrou@yahoo.co.jp>
2024-09-16 22:44:40 +0900

    nomを使って書き換え

--- src/main.rs
+++ src/main.rs
@@ -1,163 +1,86 @@
-#[derive(Debug, PartialEq)]
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{alpha1, alphanumeric1, char, multispace0},
+    combinator::recognize,
+    multi::{fold_many0, many0},
+    number::complete::recognize_float,
+    sequence::{delimited, pair},
+    IResult,
+};
+
+fn main() {
+    let input = "123";
+    println!("source: {:?}, parsed: {:?}", input, expr(input));
+
+    let input = "Hello + world";
+    println!("source: {:?}, parsed: {:?}", input, expr(input));
+
+    let input = "(123 + 456 ) + world";
+    println!("source: {:?}, parsed: {:?}", input, expr(input));
+
+    let input = "car + cdr + cdr";
+    println!("source: {:?}, parsed: {:?}", input, expr(input));
+
+    let input = "((1 + 2) + (3 + 4)) + 5 + 6";
+    println!("source: {:?}, parsed: {:?}", input, expr(input));
+}
+
+#[derive(Debug, PartialEq, Clone)]
 enum Token<'src> {
     Ident(&'src str),
     Number(f64),
-    LParen,
-    RParen,
 }
 
-#[derive(Debug, PartialEq)]
-enum TokenTree<'src> {
-    Token(Token<'src>),
-    Tree(Vec<TokenTree<'src>>),
+#[derive(Debug, PartialEq, Clone)]
+enum Expression<'src> {
+    Value(Token<'src>),
+    Add(Box<Expression<'src>>, Box<Expression<'src>>),
 }
 
-// 1文字先に進む。input[1:]を返す
-fn advance_char(input: &str) -> &str {
-    let mut chars = input.chars();
-    chars.next();
-    chars.as_str()
+fn term(i: &str) -> IResult<&str, Expression> {
+    alt((number, ident, parens))(i)
 }
 
-// 先頭の文字を読む
-fn peek_char(input: &str) -> Option<char> {
-    input.chars().next()
+fn ident(input: &str) -> IResult<&str, Expression> {
+    let (r, res) = delimited(multispace0, identifier, multispace0)(input)?;
+    Ok((r, Expression::Value(Token::Ident(res))))
 }
 
-fn whitespace(mut input: &str) -> &str {
-    while matches!(peek_char(input), Some(' ')) {
-        input = advance_char(input);
-    }
-    input
+fn identifier(input: &str) -> IResult<&str, &str> {
+    recognize(pair(
+        alt((alpha1, tag("_"))),
+        many0(alt((alphanumeric1, tag("_")))),
+    ))(input)
 }
 
-fn number(mut input: &str) -> Option<(&str, Token)> {
-    let start = input;
-    if matches!(peek_char(input), Some(_x @ ('-' | '+' | '.' | '0'..='9'))) {
-        input = advance_char(input);
-        while matches!(peek_char(input), Some(_x @ ('.' | '0'..='9'))) {
-            input = advance_char(input);
-        }
-        if let Ok(num) = start[..(start.len() - input.len())].parse::<f64>() {
-            Some((input, Token::Number(num)))
-        } else {
-            None
-        }
-    } else {
-        None
-    }
+fn number(input: &str) -> IResult<&str, Expression> {
+    let (r, v) = delimited(multispace0, recognize_float, multispace0)(input)?;
+    Ok((
+        r,
+        Expression::Value(Token::Number(v.parse().map_err(|_| {
+            nom::Err::Error(nom::error::Error {
+                input,
+                code: nom::error::ErrorKind::Digit,
+            })
+        })?)),
+    ))
 }
 
-fn ident(mut input: &str) -> Option<(&str, Token)> {
-    let start = input;
-    if matches!(peek_char(input), Some(_x @ ('a'..='z' | 'A'..='Z'))) {
-        input = advance_char(input);
-        while matches!(
-            peek_char(input),
-            Some(_x @ ('a'..='z' | 'A'..='Z' | '0'..='9'))
-        ) {
-            input = advance_char(input);
-        }
-        Some((input, Token::Ident(&start[..(start.len() - input.len())])))
-    } else {
-        None
-    }
+fn parens(i: &str) -> IResult<&str, Expression> {
+    delimited(
+        multispace0,
+        delimited(tag("("), expr, tag(")")),
+        multispace0,
+    )(i)
 }
 
-fn lparen(mut input: &str) -> Option<(&str, Token)> {
-    if matches!(peek_char(input), Some('(')) {
-        input = advance_char(input);
-        Some((input, Token::LParen))
-    } else {
-        None
-    }
-}
+fn expr(i: &str) -> IResult<&str, Expression> {
+    let (i, init) = term(i)?;
 
-fn rparen(mut input: &str) -> Option<(&str, Token)> {
-    if matches!(peek_char(input), Some(')')) {
-        input = advance_char(input);
-        Some((input, Token::RParen))
-    } else {
-        None
-    }
-}
-
-fn token(i: &str) -> Option<(&str, Token)> {
-    if let Some(res) = ident(whitespace(i)) {
-        return Some(res);
-    }
-    if let Some(res) = number(whitespace(i)) {
-        return Some(res);
-    }
-    if let Some(res) = lparen(whitespace(i)) {
-        return Some(res);
-    }
-    if let Some(res) = rparen(whitespace(i)) {
-        return Some(res);
-    }
-    None
-}
-
-fn source(mut input: &str) -> (&str, TokenTree) {
-    let mut tokens = vec![];
-    while !input.is_empty() {
-        input = if let Some((next_input, token)) = token(input) {
-            match token {
-                Token::LParen => {
-                    // 括弧の中身をTokenTreeとしてtokensに加える
-                    let (next_input, tt) = source(next_input);
-                    tokens.push(tt);
-                    next_input
-                }
-                Token::RParen => {
-                    return (next_input, TokenTree::Tree(tokens));
-                }
-                _ => {
-                    tokens.push(TokenTree::Token(token));
-                    next_input
-                }
-            }
-        } else {
-            break;
-        }
-    }
-    (input, TokenTree::Tree(tokens))
-}
-
-fn main() {
-    let input = "123 world";
-    println!("source: {}, parsed: {:?}", input, source(input),);
-    let input = "((car cdr) cdr)";
-    println!("source: {}, parsed: {:?}", input, source(input),);
-    let input = "()())))(((())))";
-    println!("source: {}, parsed: {:?}", input, source(input),);
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_whitespace() {
-        assert_eq!(whitespace("   "), "");
-        assert_eq!(whitespace("  a "), "a ");
-    }
-
-    #[test]
-    fn test_ident() {
-        assert_eq!(ident("Adam1a"), Some(("", Token::Ident("Adam1a"))));
-    }
-
-    #[test]
-    fn test_number() {
-        assert_eq!(number("123.45 "), Some((" ", Token::Number(123.45))));
-        assert_eq!(number("123.45 6"), Some((" 6", Token::Number(123.45))));
-        assert_eq!(number("Adam1a"), None);
-    }
-
-    #[test]
-    fn test_paren() {
-        assert_eq!(lparen("()"), Some((")", Token::LParen)));
-        assert_eq!(rparen(")()"), Some(("()", Token::RParen)));
-    }
+    fold_many0(
+        pair(delimited(multispace0, char('+'), multispace0), term),
+        move || init.clone(),
+        |acc, (_op, val): (char, Expression)| Expression::Add(Box::new(acc), Box::new(val)),
+    )(i)
 }
