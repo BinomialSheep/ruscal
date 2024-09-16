83f0a635f7f1a1b3c35fc9f46ce4ee96cb1fb007
BinomialSheep <jose_jinrou@yahoo.co.jp>
2024-09-16 22:56:53 +0900

    コメントと単体テストの追加

--- src/main.rs
+++ src/main.rs
@@ -38,6 +38,8 @@ enum Expression<'src> {
     Add(Box<Expression<'src>>, Box<Expression<'src>>),
 }
 
+// number, ident, parensのいずれかにマッチした場合、Ok(残りの文字列, パーサが生成した出力)を返す
+// 例：Ok(("", 123))という結果は、入力全体をパースして数値123を得たことを示す
 fn term(i: &str) -> IResult<&str, Expression> {
     alt((number, ident, parens))(i)
 }
@@ -78,9 +80,51 @@ fn parens(i: &str) -> IResult<&str, Expression> {
 fn expr(i: &str) -> IResult<&str, Expression> {
     let (i, init) = term(i)?;
 
+    // クロージャ「move || init.clone()」について：noveはキャプチャする変数の所有権の指定、||は引数なし
+
     fold_many0(
         pair(delimited(multispace0, char('+'), multispace0), term),
         move || init.clone(),
         |acc, (_op, val): (char, Expression)| Expression::Add(Box::new(acc), Box::new(val)),
     )(i)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use nom::error::ErrorKind;
+
+    #[test]
+    fn test_term_with_number() {
+        let input = "42";
+        let result = term(input);
+        assert_eq!(result, Ok(("", Expression::Value(Token::Number(42.0)))));
+    }
+
+    #[test]
+    fn test_term_with_identifier() {
+        let input = "variable";
+        let result = term(input);
+        assert_eq!(
+            result,
+            Ok(("", Expression::Value(Token::Ident("variable"))))
+        );
+    }
+
+    #[test]
+    fn test_term_with_parentheses() {
+        let input = "(123)";
+        let result = term(input);
+        assert_eq!(result, Ok(("", Expression::Value(Token::Number(123.0)))));
+    }
+
+    #[test]
+    fn test_term_with_invalid_input() {
+        let input = "!invalid";
+        let result = term(input);
+        assert!(result.is_err());
+        if let Err(nom::Err::Error(nom::error::Error { code, .. })) = result {
+            assert_eq!(code, ErrorKind::Tag);
+        }
+    }
 }
